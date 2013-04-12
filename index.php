81574c174d5af51f5e788e882fd95434bbe756b7
Marc Joly <doriatsoon@gmail.com>
2013-04-12 08:09:39 +0200

    Passage du site en utf-8

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_name('admin');
 <html xmlns="http://www.w3.org/1999/xhtml">
 
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta charset="utf-8">
         <title>Oversoon.fr</title>
         <?php include 'cssFile.php'; ?>
         <?php include 'javascriptFile.php'; ?>
