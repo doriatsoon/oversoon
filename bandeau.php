5140ca34985e9b70b271f4fbb41907b08e1a732f
Marc Joly <doriatsoon@gmail.com>
2014-08-19 20:13:31 +0200

    Modification du header

--- bandeau.php
+++ bandeau.php
@@ -1,5 +1,5 @@
 <header>
     <div id="bandeau">
-        <div id="bienvenue"><a id="bandeau_link" href="http://oversoon.free.fr/#accueil">Bienvenue sur Oversoon</a></div>
+        <div id="bienvenue"><a id="bandeau_link" href="http://oversoon.free.fr/#accueil">Bienvenue chez Marc JOLY</a></div>
     </div>
 </header>
